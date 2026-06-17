e4cf070e6c1c55bc74ecfc83552c53b61e619bad
asritha <s190289@rguktsklm.ac.in>
2026-06-17 14:19:03 +0530

    basic flask app along with it's test

---
+++ tests/test_home.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app
+
+def test_home():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.data == b"Hello, World!"
+
